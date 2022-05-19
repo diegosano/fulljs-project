fb990fe1ed843ab2fae93072af269bcb72ef8246
Diego Sano <diego.sano@nave.rs>
2022-05-18 21:49:35 -0300

    create app logic

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
-alert('Hello World');
+import { Home, Error, Contact } from './views';
+import { parseRequestURL, redirectTo, sleep } from '../src/services/utils';
+
+const routes = {
+  '/': Home,
+  '/contact': Contact,
+};
+
+const router = async () => {
+  const app = document.getElementById('app');
+
+  const request = parseRequestURL();
+
+  const parsedURL =
+    (request.resource ? `/${request.resource}` : '/') +
+    (request.id ? `/:id${request.id}` : '') +
+    (request.verb ? `/${request.verb}` : '');
+
+  const page = routes[parsedURL] ? routes[parsedURL] : Error;
+
+  app.innerHTML = await page.render();
+  await page.afterRender();
+};
+
+window.addEventListener('hashchange', router);
+window.addEventListener('load', router);
