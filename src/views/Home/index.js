f9a2a32daa35d14c90ed9956a9a790b0c1619e76
Diego Sano <diego.sano@nave.rs>
2022-06-09 00:58:49 -0300

    add sass

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,11 +1,20 @@
 import { Nav } from '../../components/Navbar';
+import img from '../../img/dracula.jpg'
 
 export const Home = {
   render: async () => {
     const view = `
     ${Nav}
-      <div>
+      <div class="container">
         <h1>Home Page</h1>
+
+        <div class="card">
+          <p class="__title">Este é o meu CARD</p>
+
+          <img class="image" src="${img}" alt="Dracula image">
+
+          <button class="button _success">Clique aqui que nada acontece</button>
+        </div>
       </div>
     `;
 
